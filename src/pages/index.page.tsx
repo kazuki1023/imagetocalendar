ef50c766dca325b3e560f53b9597ba35a1d67d98
kazuki1023 <k.keio12569@gmail.com>
2024-07-28 19:00:23 +0900

    feat: :fire: 不要なファイルを削除した

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,32 +1,19 @@
 import type { CustomNextPage } from "next";
-import dynamic from "next/dynamic";
 import Link from "next/link";
 import { Layout } from "src/layout";
-
-// chrome APIを使用するためdynamic importし、browser側でのみ読み込まれるようにする
-const Button = dynamic(
-  async () => {
-    const module = await import("src/components/Button");
-    return module.Button;
-  },
-  {
-    ssr: false,
-    loading: () => {
-      return <div className="w-10 h-4 bg-gray-100 rounded border animate-pulse"></div>;
-    },
-  },
-);
+import FileUploader from "src/components/fileUploader";
 
 const IndexPage: CustomNextPage = () => {
   return (
     <div>
       <h1 className="text-2xl font-bold whitespace-nowrap">Chrome Extension Template</h1>
-      <Button />
-      <div>
-        <Link href="/sample">
-          <a className="text-blue-500 underline">to sample page</a>
-        </Link>
-      </div>
+      <FileUploader
+        label="Upload your files"
+        labelAlt="Drag and drop your files here"
+        acceptedFileTypes={['.jpg', '.jpeg', '.png']}
+        allowMultiple
+        url="http://localhost:3000/api/upload"
+      />
     </div>
   );
 };
