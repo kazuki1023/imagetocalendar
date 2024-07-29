7acb0095227b542fca94dfc7e8a041bff029f5c0
kazuki1023 <k.keio12569@gmail.com>
2024-07-30 00:48:49 +0900

    feat: :fire: 不要なconsoleを削除

--- src/hooks/useGoogleCalendar.ts
+++ src/hooks/useGoogleCalendar.ts
@@ -12,7 +12,6 @@ const useGoogleCalendar = () => {
 
     // 開始日時のDateオブジェクトを作成
     const startDate = formatStartDate(date, start_time);
-    console.log(startDate);
     const endDateTime = formatEndDate(date, start_time, required_time);
     const calendarUrl = new URL("https://calendar.google.com/calendar/render");
     calendarUrl.searchParams.set("action", "TEMPLATE");
