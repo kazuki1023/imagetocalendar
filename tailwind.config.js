45363ebce078c97d9485036c963b984d55987738
kazuki1023 <k.keio12569@gmail.com>
2024-07-29 23:45:45 +0900

    style: :zap: format整えた

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,18 @@ module.exports = {
   variants: { extend: {} },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#38BDF8",
-          secondary: "#818CF8",
-          accent: "#F471B5",
-          neutral: "#1E293B",
-          "base-100": "#0F172A",
-          info: "#0CA5E9",
-          success: "#2DD4BF",
-          warning: "#F4BF50",
-          error: "#FB7085",
-        },
+    themes: {
+      mytheme: {
+        primary: "#38BDF8",
+        secondary: "#818CF8",
+        accent: "#F471B5",
+        neutral: "#1E293B",
+        "base-100": "#0F172A",
+        info: "#0CA5E9",
+        success: "#2DD4BF",
+        warning: "#F4BF50",
+        error: "#FB7085",
       },
-    ],
+    },
   },
 };
