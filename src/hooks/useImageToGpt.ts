a7dfdbd6c916442ce59bfa54937961ec83fe02aa
kazuki1023 <k.keio12569@gmail.com>
2024-07-29 15:47:25 +0900

    feat: :sparkles:

--- src/hooks/useImageToGpt.ts
+++ src/hooks/useImageToGpt.ts
@@ -1,20 +1,22 @@
 import { useState } from "react";
+import getBaseUrl from "src/utils/getBaseUrl";
 
 const useImageToGPT = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [response, setResponse] = useState<any>(null);
+  const baseUrl = getBaseUrl();
 
   const sendImageToGPT = async (base64Image: string) => {
     setLoading(true);
     setError(null);
 
     try {
-      const res = await fetch("http://localhost:3000/api/imageToGPT", {
+      const res = await fetch(`${baseUrl}/api/imageToGPT`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": "Bearer " + process.env.NEXT_PUBLIC_AUTH_TOKEN,
+          Authorization: "Bearer " + process.env.NEXT_PUBLIC_AUTH_TOKEN,
         },
         body: JSON.stringify({ base64Image }),
       });
