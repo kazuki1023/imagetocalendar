62a366679e0fb2b959160c152c6c4cb54c537bae
kazuki1023 <k.keio12569@gmail.com>
2024-07-29 09:08:14 +0900

    feat: :sparkles: styleを修正し、uploadされた画像の情報を取得できるようにした

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,17 +1,43 @@
 import type { CustomNextPage } from "next";
+import { useState } from "react";
 import DropImageZone from "src/components/DropImageZone";
 import { Layout } from "src/layout";
 
 const IndexPage: CustomNextPage = () => {
+
+  const [image, setImage] = useState<string | null>(null);
+  const onDropFile = (file: File) => {
+    if (file.type.substring(0, 5) !== "image") {
+      alert("画像ファイルでないものはアップロードできません！");
+    } else {
+      const fileReader = new FileReader();
+      fileReader.onload = () => {
+        const imageSrc: string = fileReader.result as string;
+        setImage(imageSrc);
+      };
+      fileReader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div>
       <h1 className="text-2xl font-bold whitespace-nowrap">Chrome Extension Template</h1>
-      <DropImageZone onDropFile={(file) => console.log(file)}>
-        <div className="flex justify-center items-center w-96 h-96 bg-gray-100">
-          <p>Drop image here</p>
-        </div>
-      </DropImageZone>
-    </div>
+      <div className="w-96 h-96">
+        {
+          image ? (
+            <div className="overflow-hidden w-full h-full">
+              <img src={image} alt="uploaded" className="object-cover hover:object-scale-down w-full h-full " />
+            </div>
+          ) : (
+            <DropImageZone onDropFile={onDropFile}>
+              <div className="flex justify-center items-center w-full h-full bg-gray-100">
+                <p>Drop image here</p>
+              </div >
+            </DropImageZone >
+          )
+        }
+      </div>
+    </div >
   );
 };
 
