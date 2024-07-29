4a0a72253b02e04a521bf580bf0c546e9b4d69aa
kazuki1023 <k.keio12569@gmail.com>
2024-07-29 19:07:04 +0900

    feat: :sparkles: 終了時間をformatする関数を作成して、それを呼び出した

--- src/hooks/useGoogleCalendar.ts
+++ src/hooks/useGoogleCalendar.ts
@@ -1,11 +1,9 @@
-import formatStartDate from "src/utils/formatStartDate";
+import formatEndDate from "../utils/formatEndDate";
+import formatStartDate from "../utils/formatStartDate";
 
 const useGoogleCalendar = () => {
   const createEvent = (eventData: any) => {
     const { date, start_time, required_time, title, client } = eventData;
-
-    console.log(date);
-    console.log(start_time);
 
     if (!date) {
       console.error("Date is required to create an event.");
@@ -14,14 +12,8 @@ const useGoogleCalendar = () => {
 
     // 開始日時のDateオブジェクトを作成
     const startDate = formatStartDate(date, start_time);
-
-    const effectiveRequiredTime = required_time ? parseInt(required_time) : 60;
-    const endDate = new Date(
-      new Date(`${date} ${start_time}`).getTime() + effectiveRequiredTime * 60000,
-    );
-    const endDateTime = formatStartDate(date, endDate.toTimeString().slice(0, 5));
-    console.log(endDateTime);
-
+    console.log(startDate);
+    const endDateTime = formatEndDate(date, start_time, required_time);
     const calendarUrl = new URL("https://calendar.google.com/calendar/render");
     calendarUrl.searchParams.set("action", "TEMPLATE");
     calendarUrl.searchParams.set("text", title + "" + client);
